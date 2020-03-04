bb20e17ae37d0ebff451c7b2ef7ae6d148885f42
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2020-03-04 19:06:41 +0000

    Get the computer's choice and log it to the console

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,6 +19,41 @@
   //
 
   /**
+   * Randomly shuffle an array
+   * https://stackoverflow.com/a/2450976/1293256
+   * @param  {Array} array The array to shuffle
+   * @return {String}      The first item in the shuffled array
+   */
+  var shuffle = function ( array ) {
+
+    var currentIndex = array.length;
+    var temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while ( 0 !== currentIndex ) {
+      // Pick a remaining element...
+      randomIndex = Math.floor( Math.random() * currentIndex );
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[ currentIndex ];
+      array[ currentIndex ] = array[ randomIndex ];
+      array[ randomIndex ] = temporaryValue;
+    }
+
+    return array;
+
+  };
+
+  /**
+   * Make the computer's choice
+   * @return {String} The choice (rock/paper/scissors)
+   */
+  var makeChoice = function() {
+    return shuffle( choices.slice() )[ 0 ];
+  };
+
+  /**
    * Handle click events
    * @param {Object} event The Event interface 
    */
@@ -30,6 +65,10 @@
     // Get the user's choice
     var userChoice = event.target.getAttribute( "data-choice" );
     console.log( userChoice );
+
+    // Get the computer's choice
+    var computerChoice = makeChoice();
+    console.log( computerChoice );
 
   };
 
